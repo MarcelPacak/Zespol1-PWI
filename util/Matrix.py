460963f53a06164220df03941f5fc7c3034fdb93
MarcelPacak <m2p13cak7@MarcelPacak.localdomain>
2025-12-05 21:00:07 +0100

    Utworzenie klasy Matrix i Vector w paczce util

---
+++ util/Matrix.py
@@ -0,0 +1,14 @@
+from .Vector import Vector
+
+class Matrix:
+    def __init__(self, a: int, b: int, c: int, d: int):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def times(self, vector: Vector) -> Vector:
+        new_vector = Vector(0, 0)
+        new_vector.x = self.a * vector.x + self.b * vector.y
+        new_vector.y = self.c * vector.x + self.d * vector.y
+        return new_vector
