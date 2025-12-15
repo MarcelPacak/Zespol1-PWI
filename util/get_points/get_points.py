a6d9c6fb957ba8c2b006e6873449ef4517f4687d
MarcelPacak <m2p13cak7@MarcelPacak.localdomain>
2025-12-15 14:47:08 +0100

    Napisane funkcji get_zeros

---
+++ util/get_points/get_points.py
@@ -0,0 +1,15 @@
+# TEST
+class Polynomial:
+    def get_value(x, y):
+        return x * y**3 + x**2 * y + 1
+
+class PolymonialFunction:
+    degree = 3
+    def get_value(x):
+        return -3 * x**3 + 9 * x + 0.5
+# TEST
+
+from Vector import Vector
+
+def get_points(polynomial: Polynomial, left_down_corner: Vector, right_up_corner: Vector, epsilon: float, ) -> list:
+    signs = []
---
+++ util/get_points/get_zeros.py
@@ -0,0 +1,32 @@
+def close_to_zero(value: float, epsilon: float) -> bool:
+    return value <= epsilon and value >= -1 * epsilon
+
+# f(p) MUST be negative and f(q) MUST be positive, otherwise it's UB
+def get_zero(function: PolymonialFunction, p: float, q: float, epsilon: float) -> float:
+    pivot = (p + q) / 2
+    value = function.get_value(pivot)
+
+    if(close_to_zero(value, epsilon)):
+        return pivot
+    if(value > 0):
+        return get_zero(function, p, pivot, epsilon)
+    return  get_zero(function, pivot, q, epsilon)
+
+def get_zeros(function: PolymonialFunction, p: float, q: float, epsilon: float, sections: int) -> list:
+    delta = (q - p) / sections
+    zeros = []
+    ptr_old = p
+    value_old = function.get_value(ptr_old)
+
+    for i in range(0, sections):
+        ptr_new = ptr_old + delta
+        value_new = function.get_value(ptr_new)
+        if(value_old < 0 and value_new > 0):
+            zeros.append(get_zero(function, ptr_old, ptr_new, epsilon))
+        elif(value_old > 0 and value_new < 0):
+            zeros.append(get_zero(function, ptr_new, ptr_old, epsilon))
+
+        ptr_old = ptr_new
+        value_old = value_new
+
+    return zeros
