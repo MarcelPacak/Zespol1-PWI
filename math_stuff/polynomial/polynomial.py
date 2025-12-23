0562de077e5cf87e1f9cf92f681e025765285654
pawel <pawel.czujowski@gmail.com>
2025-12-23 14:49:27 +0100

    dodanie klasy PolynomialFunction i poprawienie logiki setx sety

--- math_stuff/polynomial/polynomial.py
+++ math_stuff/polynomial/polynomial.py
@@ -1,10 +1,19 @@
+class OneElement:
+    def __init__(self, coeff, x_exp):
+        self.coeff = coeff
+        self.x_exp = x_exp
+
+class PolynomialFunction:
+    def __init__(self, list_of_elements, x : float):
+        self.list_of_elements = list_of_elements
+        self.x = x
+
+
 class Elements:
     def __init__(self, coeff, x_exp, y_exp): # przykladowo dla 3*x^2*y^1 (3, 2, 1)
         self.coeff = coeff
         self.x_exp = x_exp
         self.y_exp = y_exp
-
-# class PolynomialFunction:
 
 class Polynomial:
     def __init__(self, list_of_elements):
@@ -18,16 +27,17 @@ class Polynomial:
 
 
     def set_x(self, x: float): #zwracanie wielomianu jednej zmiennej x lub y
-        PolynomialFunction = []
+        list_of_set_x = []
         for coeff, x_exp, y_exp in self.list_of_elements:
-            new_element = Elements(coeff * (x ** x_exp), 0, y_exp)
-            PolynomialFunction.append(new_element)
-        return PolynomialFunction
-    #zwracany obiekt ma miec nazwe PolynomialFunction
+            new_element = OneElement(coeff * (x ** x_exp), y_exp)
+            list_of_set_x.append(new_element)
+        PolynomialFunctionX = PolynomialFunction(list_of_set_x, x)
+        return PolynomialFunctionX
 
     def set_y(self, y: float): 
-        PolynomialFunction = []
+        list_of_set_y = []
         for coeff, x_exp, y_exp in self.list_of_elements:
-            new_element = Elements(coeff * (y ** y_exp), x_exp, 0)
-            PolynomialFunction.append(new_element)
-        return PolynomialFunction
+            new_element = OneElement(coeff * (y ** y_exp), x_exp)
+            list_of_set_y.append(new_element)
+        PolynomialFunctionY = PolynomialFunction(list_of_set_y, y)
+        return PolynomialFunctionY
