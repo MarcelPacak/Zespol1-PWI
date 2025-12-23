08105eaa16e0942865a477ba0993f7d1bd38822a
pawel <pawel.czujowski@gmail.com>
2025-12-23 14:08:31 +0100

    poprawki w setx sety

--- math_stuff/polynomial/polynomial.py
+++ math_stuff/polynomial/polynomial.py
@@ -4,11 +4,13 @@ class Elements:
         self.x_exp = x_exp
         self.y_exp = y_exp
 
+# class PolynomialFunction:
+
 class Polynomial:
     def __init__(self, list_of_elements):
         self.list_of_elements = list_of_elements
     
-    def calc_polynomial(self, x: float, y: float):
+    def get_value(self, x: float, y: float):
         result = 0
         for coeff, x_exp, y_exp in self.list_of_elements:
             result += coeff * (x ** x_exp) * (y ** y_exp)
@@ -18,7 +20,7 @@ class Polynomial:
     def set_x(self, x: float): #zwracanie wielomianu jednej zmiennej x lub y
         PolynomialFunction = []
         for coeff, x_exp, y_exp in self.list_of_elements:
-            new_element = Elements(coeff + (x ** x_exp), 0, y_exp)
+            new_element = Elements(coeff * (x ** x_exp), 0, y_exp)
             PolynomialFunction.append(new_element)
         return PolynomialFunction
     #zwracany obiekt ma miec nazwe PolynomialFunction
@@ -26,6 +28,6 @@ class Polynomial:
     def set_y(self, y: float): 
         PolynomialFunction = []
         for coeff, x_exp, y_exp in self.list_of_elements:
-            new_element = Elements(coeff + (y ** y_exp), x_exp, 0)
+            new_element = Elements(coeff * (y ** y_exp), x_exp, 0)
             PolynomialFunction.append(new_element)
         return PolynomialFunction
